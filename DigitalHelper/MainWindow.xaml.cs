1e45b0c8282238c51ee10b6953ca4bb128699cbb
ben <bkk220000@utdallas.edu>
2025-10-27 16:11:33 -0500

    Fix design-time frame rendering and use direct image source

--- DigitalHelper/MainWindow.xaml.cs
+++ DigitalHelper/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace DigitalHelper
         public MainWindow()
         {
             InitializeComponent();
+            MainFrame.Content = null;
+            MainFrame.Source = new Uri("/Views/MainMenu.xaml", UriKind.Relative);
             MainFrame.Navigate(new Uri("/Views/MainMenu.xaml", UriKind.Relative));
         }
 
